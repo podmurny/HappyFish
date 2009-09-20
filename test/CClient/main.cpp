cc60abdb56d433cf02fabccad273e4b79cf2743a
Vyacheslav Podmurny <Podmurny@g-sg.net>
2009-09-20 21:18:11 +0300

    Alesx homework

--- test/CClient/main.cpp
+++ test/CClient/main.cpp
@@ -4,7 +4,12 @@ int main()
 {
 
         CClient client;
-	std::string st;
-	st=client.get_date_time();
-	return 0;
+//	std::string st;
+//	st=client.get_date_time();
+//        std::string st("slava");
+//        std::string st1("hello world");
+//        client.write_message_to_file(st,st1);
+        std::string str;
+        str = client.read_message_from_file();
+        return 0;
 }
