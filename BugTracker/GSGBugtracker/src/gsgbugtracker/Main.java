43155b068c63c06078bab21fdfeadea8f0114a1f
Vyacheslav Podmurny <Podmurny@g-sg.net>
2009-10-07 11:59:53 +0300

    Add main form into BugTracker

--- BugTracker/GSGBugtracker/src/gsgbugtracker/Main.java
+++ BugTracker/GSGBugtracker/src/gsgbugtracker/Main.java
@@ -2,20 +2,25 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package gsgbugtracker;
 
 /**
  *
  * @author slava
  */
-public class Main {
+public
+        class Main
+{
+    private static
+    MainForm mainform;
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static
+            void main(String[] args)
+    {
+        mainform = new MainForm();
+        mainform.setVisible(true);
     }
-
 }
