1371aab0f510993c23144909c544f88180a11b60
tristam <usanenko_vadim@ukr.net>
2009-10-15 22:01:09 +0300

    Yup, this is cursed commit, but now it must work

--- BugTracker/GSGBugtracker/src/bugtracker/CBugTrack.java
+++ BugTracker/GSGBugtracker/src/bugtracker/CBugTrack.java
@@ -8,23 +8,44 @@ package bugtracker;
  *
  * @author slava
  */
+
+    import java.sql.SQLException;
+    import java.util.logging.Level;
+    import java.util.logging.Logger;
+
 public class CBugTrack
 {
 
-    private boolean login;
+    java.sql.Connection connection = null;
+    java.sql.Statement statement;
+    java.sql.ResultSet result;
+    static String host = null;
+    static String port = null;
+    static String login = null;
+    static String password = null;
+    static boolean default_port = true;
+
+    private int access_level; //TODO
+    //private boolean login; Зачем?
     // about access_level //
     // 0 - guest          //
     // 1 - tester         //
     // 2 - developer      //
     // 3 - lead developer //
     // 4 - project manager//
-    private int access_level;
+    
     //constructor
-
-    public CBugTrack(String IP, String password)
+    public CBugTrack(String login, String password, String host)
     {
-        login = false;
-        access_level = 0;
+        access_level = 0; //TODO
+        CBugTrack.login = login;
+        String database = "ProjectManagement";
+        String url = "jdbc:mysql://" + host + "/" + database;
+        try {
+            connection = java.sql.DriverManager.getConnection(url, login, password);
+        } catch (SQLException ex) {
+            Logger.getLogger(CBugTrack.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     //////////////////methods///////////////////////
     //login method 
@@ -34,4 +55,29 @@ public class CBugTrack
         //add here connection to database and login check
         return 0;
     }
+
+    public int get_all_project_info() {
+
+       String[] ProjectName;
+       String[] ProjectInfo;
+       String[] Tasks;
+       String[] Bugs;
+
+        try {
+           statement = connection.createStatement();
+        } catch (SQLException ex) {
+           Logger.getLogger(CBugTrack.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            statement.executeQuery("SELECT DISTINCT Task.ProjectName, Task.Task, Task.TaskInfo, Task.Priority, Task.Status, Project.ProjectInfo FROM Task INNER JOIN Project ON Task.ProjectName = Project.ProjectName WHERE Task.Username='" + login + "");
+        } catch (SQLException ex) {
+            Logger.getLogger(CBugTrack.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            result = statement.getResultSet();
+        } catch (SQLException ex) {
+            Logger.getLogger(CBugTrack.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
+     }
 }
